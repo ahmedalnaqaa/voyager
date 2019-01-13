927dab66a99988a853ff2b100c4a920939d5f049
Ahmed Alnaqaa <ahmedalnaqaa@gmail.com>
2019-01-13 03:57:32 +0200

    Remove unused directory

--- middlewares/user.js
+++
@@ -1,8 +0,0 @@
-const oneSignalService = require('../services/onesignal');
-
-// update user player ID
-exports.setPlayerId = function () {
-    oneSignalService.createClient().then((res) => {
-        res.data.id;
-    });
-};
