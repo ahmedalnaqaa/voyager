f360bcdef3e362ff43eff46cf848553be76b68a4
Ahmed Alnaqaa <ahmedalnaqaa@gmail.com>
2019-01-20 04:30:16 +0200

    Run test on docker

--- test/user_test.js
+++ test/user_test.js
@@ -1,6 +1,6 @@
 //During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
-require('dotenv').config({ path: require('path').resolve(__dirname, '../config/vars/'+process.env.APP_ENV+'.env') });
+require('dotenv').config({ path: require('path').resolve(__dirname, '../config/vars/'+process.env.NODE_ENV+'.env') });
 
 const db = require('../config/db');
 const User = require('../models/user');
